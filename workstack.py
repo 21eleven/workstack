8552445444d3e17731b6f2e2fdf3073d43b9e3a3
Noah Lidell <noahlidell@gmail.com>
2020-01-13 12:58:23 -0600

    init class

---
+++ workstack.py
@@ -0,0 +1,23 @@
+import os
+
+
+class workstack():
+    def __init__(self, a):
+        self.a = a
+
+    def print(self):
+        print(self.a)
+
+    def __repr__(self):
+        return f"workstack({self.a})"
+
+    # i should have functions that track some data points for the day
+    # units of alcohol consumed, mood, times I ate out, time spent watching tv,
+    # time spent
+
+
+def chk():
+    return 44
+
+
+ws = workstack
